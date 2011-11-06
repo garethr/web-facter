e6c273c583e7d3ecf6d329d4ad80b2856b22110d
Gareth Rushgrove <gareth@morethanseven.net>
2011-11-06 17:46:07 +0000

    add sample puppet module for managing web-facter

---
+++ puppet/modules/web-facter/manifests/init.pp
@@ -0,0 +1,28 @@
+class webfacter {
+
+  package { "web-facter":
+    ensure   => installed,
+    provider => gem,
+  }
+
+  file { "/etc/web-facter.conf":
+    ensure  => present,
+    content => template('web-facter/web-facter.conf'),
+    notify  => Service['web-facter'],
+  }
+
+  file { "/etc/init/web-facter":
+    ensure  => present,
+    source  => "puppet:///modules/web-facter/web-facter",
+  }
+
+  service { "web-facter":
+    ensure   => running,
+    provider => upstart,
+    require  => [
+      Package["web-facter"],
+      File["/etc/init/web-facter"],
+    ]
+  }
+
+}
